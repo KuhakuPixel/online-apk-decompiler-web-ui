a7d4ae0544bac30ca814331c583836f371ffdb50
kuhakupixel <nicholaspixels@gmail.com>
2023-10-31 22:21:54 +0700

    initial commit

---
+++ src/main/java/com/kuhakupixel/onlinedecompiler/OnlinedecompilerApplication.java
@@ -0,0 +1,20 @@
+package com.kuhakupixel.onlinedecompiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+@RestController
+public class OnlinedecompilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinedecompilerApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/kuhakupixel/onlinedecompiler/OnlinedecompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuhakupixel.onlinedecompiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinedecompilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
