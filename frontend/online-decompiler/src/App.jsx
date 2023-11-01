cdd0486f3bd3905f6e65a4e8ba39d4eadfa5f06e
kuhakupixel <nicholaspixels@gmail.com>
2023-11-01 23:53:05 +0700

    set to dark theme

--- frontend/online-decompiler/src/App.jsx
+++ frontend/online-decompiler/src/App.jsx
@@ -2,28 +2,41 @@ import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+
+
 
 function UploadApk() {
 
   return (
-    <div>
+    <div >
       <iframe name="dummyframe" id="dummyframe" style={{ display: "none" }}></iframe>
 
-      <form method="POST" enctype="multipart/form-data" action="http://localhost:8080/apk" target="dummyframe">
-        <table>
-          <tr><td>File to upload:</td><td><input type="file" name="file" /></td></tr>
-          <tr><td><input type="submit" value="Upload" /></td></tr>
-        </table>
-      </form>
+      <Form method="POST" enctype="multipart/form-data" action="http://localhost:8080/apk" target="dummyframe">
+        <Form.Group controlId="formFile" className="mb-3" >
+          <Form.Label>Choose apk file and press upload</Form.Label>
+          <Form.Control type="file" name="file" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Upload
+        </Button>
+
+      </Form>
     </div>
   )
 }
 function App() {
   const [count, setCount] = useState(0)
+  document.documentElement.setAttribute("data-bs-theme", "dark")
 
   return (
     <>
-      <div >
+      <div style={{ padding: "20px" }}>
         <h1> Online Apk Decompiler</h1>
 
         <UploadApk></UploadApk>
