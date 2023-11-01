83ddc017f372481ad683b35e8ed636dc29aafae4
kuhakupixel <nicholaspixels@gmail.com>
2023-11-01 17:48:36 +0700

    add frontend and some file upload size limit

---
+++ frontend/online-decompiler/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+
+function UploadApk() {
+
+  return (
+    <div>
+      <iframe name="dummyframe" id="dummyframe" style={{display:"none"}}></iframe>
+
+      <form method="POST" enctype="multipart/form-data" action="http://localhost:8080/apk" target="dummyframe">
+        <table>
+          <tr><td>File to upload:</td><td><input type="file" name="file" /></td></tr>
+          <tr><td></td><td><input type="submit" value="Upload" /></td></tr>
+        </table>
+      </form>
+    </div>
+  )
+}
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div >
+        <h1> Online Apk Decompiler</h1>
+
+        <UploadApk></UploadApk>
+      </div>
+    </>
+  )
+}
+
+export default App
--- src/main/java/com/kuhakupixel/onlinedecompiler/OnlinedecompilerApplication.java
+++ src/main/java/com/kuhakupixel/onlinedecompiler/OnlinedecompilerApplication.java
@@ -1,10 +1,14 @@
 package com.kuhakupixel.onlinedecompiler;
 
 import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 @SpringBootApplication
 @RestController
 public class OnlinedecompilerApplication {
+
+	List<MyData> datas = new ArrayList<MyData>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlinedecompilerApplication.class, args);
@@ -23,9 +29,37 @@ public class OnlinedecompilerApplication {
 		return String.format("Hello %s!", name);
 	}
 
-	@PostMapping("/employees")
-	void newData(@RequestBody MyData myData) {
+	@PostMapping("/datas")
+	MyData newData(@RequestBody MyData myData) {
 		System.out.println("data name is : " + myData.name);
+		datas.add(myData);
+		return myData;
 
+	}
+
+	@GetMapping("/datas")
+	List<MyData> getDatas() {
+
+		return datas;
+
+	}
+
+	@GetMapping("/datas/{name}")
+	MyData getData(@RequestParam(value = "name") String name) {
+		for (MyData data : datas) {
+			if (data.name.equals(name)) {
+				return data;
+			}
+		}
+
+		return null;
+	}
+
+	@PostMapping("/apk")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+			RedirectAttributes redirectAttributes) {
+
+		System.out.println("File name: " + file.getName());
+		return "redirect:/";
 	}
 }
