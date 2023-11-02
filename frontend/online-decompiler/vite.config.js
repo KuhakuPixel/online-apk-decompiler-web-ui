d68dfd24b9165d9fbd8c8935c414fa565c6c3b4e
kuhakupixel <nicholaspixels@gmail.com>
2023-11-02 18:34:47 +0700

    add docker for frontend

--- frontend/online-decompiler/vite.config.js
+++ frontend/online-decompiler/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // needed for the Docker Container port mapping to work
+    port: 8000, // you can replace this port with any port
+  }
 })
