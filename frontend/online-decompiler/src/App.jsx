d0853b37ded4e731efd843cf720823f5cb66eed3
kuhakupixel <nicholaspixels@gmail.com>
2023-11-02 15:26:37 +0700

    remove uncessary headers in fetch

--- frontend/online-decompiler/src/App.jsx
+++ frontend/online-decompiler/src/App.jsx
@@ -40,20 +40,6 @@ function UploadApk() {
     */
 
     const response = await fetch("http://localhost:8080/apk", {
-      "headers": {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-        "accept-language": "en-US,en;q=0.9",
-        "cache-control": "max-age=0",
-        "sec-ch-ua": "\"Not/A)Brand\";v=\"99\", \"Brave\";v=\"115\", \"Chromium\";v=\"115\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Linux\"",
-        "sec-fetch-dest": "iframe",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-site",
-        "sec-fetch-user": "?1",
-        "sec-gpc": "1",
-        "upgrade-insecure-requests": "1"
-      },
       "referrer": "http://localhost:5173/",
       "referrerPolicy": "strict-origin-when-cross-origin",
       "method": "POST",
